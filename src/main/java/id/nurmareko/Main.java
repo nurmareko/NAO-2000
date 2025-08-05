76c15c633e0822b9d49e76a9a058f04071a33b5a
nurmareko <nurmareko@yandex.ru>
2025-08-05 21:20:43 +0700

    Add dummy bottom content display

--- src/main/java/id/nurmareko/Main.java
+++ src/main/java/id/nurmareko/Main.java
@@ -1,10 +1,12 @@
 package id.nurmareko;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,8 +22,18 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    private Node createBottom() {
+        Label fileType = new Label("FLAC");
+        Label bitrate = new Label("1058 kbps");
+        Label frequency = new Label("44100 Hz");
+        Label trackProgress = new Label("0:03 / 4:57");
+        HBox results = new HBox(20, fileType, bitrate, frequency, trackProgress);
+        return results;
+    }
+
     private Region createContent() {
-        VBox results = new VBox(new Label("NAO-2000"));
+        BorderPane results = new BorderPane();
+        results.setBottom(createBottom());
         return results;
     }
 }
